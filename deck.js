acba0893251b43383cb37b5ea70d5f0848b4859c
Juan Caicedo <retiredcanadianpoet@gmail.com>
2018-10-11 08:13:32 -0500

    develop individual slide

---
+++ deck.js
@@ -0,0 +1,6 @@
+import { CodeSurfer } from 'mdx-deck-code-surfer';
+export { default as theme } from './juan-theme';
+import nightOwl from 'prism-react-renderer/themes/nightOwl';
+import one from './01.mdx';
+
+export default [...one];
