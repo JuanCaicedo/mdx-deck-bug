af09b67bf4da71a572ba801790a147437383ef1b
Juan Caicedo <retiredcanadianpoet@gmail.com>
2018-10-11 07:58:37 -0500

    add theme

---
+++ juan-theme.js
@@ -0,0 +1,16 @@
+import theme from 'mdx-deck/themes';
+
+export default {
+  // extends the default theme
+  ...theme,
+  // add a custom font
+  font: 'Futura',
+  // custom colors
+  colors: {
+    text: '#24292F',
+    background: '#57C1FF',
+    link: 'yellow',
+    code: '#24292F',
+    codeBackground: '#F3F4F4'
+  }
+};
